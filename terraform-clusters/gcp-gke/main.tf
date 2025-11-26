cf9388ea1c526a578533a488500c4a75e2085af6
faly9 <randriantsoafaly8@gmail.com>
2025-11-26 13:36:14 +0000

    terraform

--- terraform-clusters/gcp-gke/main.tf
+++ terraform-clusters/gcp-gke/main.tf
@@ -1,12 +1,24 @@
-resource "google_container_cluster" "crud" {
- name = var.cluster_name
- location = var.region
- initial_node_count = var.node_count
- node_config {
- machine_type = "e2-medium"
- oauth_scopes = [
- "https://www.googleapis.com/auth/cloud-platform",
- ]
- }
- remove_default_node_pool = false
+resource "google_container_cluster" "gke_cluster" {
+  name     = var.cluster_name
+  location = var.zone
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  maintenance_policy {
+    recurring_window {
+      window {
+        days_of_week = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+        start_time    = "00:00"
+        end_time      = "23:59"
+      }
+    }
+  }
+
+  network    = "default"
+  subnetwork = "default"
 }
--- terraform-clusters/gcp-gke/provider.tf
+++ terraform-clusters/gcp-gke/provider.tf
@@ -1,10 +1,16 @@
-provider "google" {
- project = var.project_id
- region = var.region
-}
 terraform {
- backend "gcs" {
- bucket = "tf-state-gke"
- prefix = "terraform/state"
- }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.17.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
 }
